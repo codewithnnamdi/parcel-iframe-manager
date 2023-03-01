a63580fbdf31dff38fd5e3663933129320fcaa05
Nnamdi Michael Okpala <codewithnnamdi@proton.me>
2023-03-01 08:03:31 +0000

    a

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const { Transformer } = require('@parcel/core');
+const { promisify } = require('util');
+const iframe = promisify(require('iframe-resizer').iframeResizer);
+
+/**
+ * An IframeManager
+ */
+class IframeManager extends Transformer {
+    async transform() {
+        const iframes = this.getAssets().filter(asset => asset.type === 'html' && asset.content.includes('<iframe'));
+        const promises = iframes.map(async iframeAsset => {
+            const iframeContent = await iframeAsset.getCode();
+            const iframeResized = await iframe({
+                iframeResizerOptions: {
+                    log: false,
+                    checkOrigin: false,
+                    resizeFrom: 'child',
+                    heightCalculationMethod: 'bodyOffset'
+                }
+            }, iframeAsset, iframeContent);
+            return iframeResized;
+        });
+        await Promise.all(promises);
+        return [this.getAsset()];
+    }
+}
+
